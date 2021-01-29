10cf044d8541759193b944f9f8339d028963bcda
Vivek Chavda <41143320+vivekchavda2001@users.noreply.github.com>
2021-01-29 22:50:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,16 @@ var EmployeeRouter = require('./routers/Employees')
 var url = "mongodb+srv://vivekchavda:123@cluster0.sbq38.mongodb.net/Employee?retryWrites=true&w=majority"
 app.use(bodyParser.urlencoded({extended:true})) 
 app.use(express.json())
-mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true,useFindAndModify: true})
-var con  = mongoose.connection;
-app.use('/',EmployeeRouter)
-con.on('open',()=>{
-    console.log("Connected")
-    app.listen(process.env.PORT || 9000,(err)=>{
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true,useFindAndModify: true}).then(()=>{
+	app.use('/',EmployeeRouter)
+	app.listen(process.env.PORT || 9000,(err)=>{
         if(err)
         {
         console.log(err);
         }
         console.log('Server started')
     })
-})
+});
+
+
 
