a7173a7000870e49e0622ef80d22112beb3f28a0
vivekchavda2001 <vchavda446@rku.ac.in>
2021-03-19 22:26:15 +0530

    CORS Added to index.js

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var express = require('express')
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 var app = express();
+const cors = require('cors');
 var EmployeeRouter = require('./routers/Employees')
 var url = "mongodb+srv://vivekchavda:123@cluster0.sbq38.mongodb.net/Employee?retryWrites=true&w=majority"
 app.use(bodyParser.urlencoded({extended:true})) 
 app.use(express.json())
+app.use(cors())
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true,useFindAndModify: true}).then(()=>{
 	app.use('/',EmployeeRouter)
 	app.listen(process.env.PORT || 9000,(err)=>{
